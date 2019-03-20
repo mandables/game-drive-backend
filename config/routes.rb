181e13bd034d0fd8d4d0ed63c39707377de9d1c2
amaan19 <amaan19@hotmail.com>
2019-03-20 13:14:57 +0000

    Added sign-in user token

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
       resources :users
     end
   end
+
+  post "signin", to: "api/v1/users#signin"
+  get "validate", to: "api/v1/users#validate"
+  post "games", to: "api/v1/users#get_games"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
